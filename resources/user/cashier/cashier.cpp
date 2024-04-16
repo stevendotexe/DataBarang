2cc074e4d95bf788003fb72e911d3a72d22dafa4
kintadsvn <kintaaaw@gmail.com>
2024-04-16 07:01:10 +0700

    Fix function transaction in cashier.cpp n delete file transaction.cpp

--- resources/user/cashier/cashier.cpp
+++ resources/user/cashier/cashier.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 #include <iomanip>
-#include <ctime>
+#include <fstream>
+#include <sstream>
 #include <string>
+#include <vector>
+
 using namespace std;
+
+struct item {
+    string id_item;
+    string item_name;
+    string item_price;
+};
 
 /* Create Id Transaction */
 void create_id_transaction(){
@@ -50,8 +59,65 @@ void current_dateTime(){
     /* End time section */
 };
 
+vector<item> open_file_item(){
+    ifstream file("D:\\Tugas Kuliah\\Semester 2\\RL105 - Struktur Data dan Algoritma\\DataBarang\\database\\item.csv", ios::in);
+
+    if (!file.is_open()) {
+        cout << "Gagal membuka file CSV" << endl;
+    }
+
+    string line;
+    vector<item> data;
+
+    // Baca setiap baris dalam file CSV
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string id, name, price_str;
+
+        // Pisahkan setiap kolom dalam baris
+        getline(ss, id, ',');
+        getline(ss, name, ',');
+        getline(ss, price_str, ',');
+
+        // Buat objek Barang dan tambahkan ke vektor data
+        item Item = {id, name, price_str};
+        data.push_back(Item);
+    }
+
+    // Tutup file
+    file.close();
+
+    return data;
+}
+
+void transaction(){
+    vector<item> data = open_file_item();
+
+    // Meminta pengguna untuk memasukkan ID barang
+    string input_id;
+    cout << "ID Barang: ";
+    cin >> input_id;
+
+    // Mencari barang berdasarkan ID
+    bool ditemukan = false;
+    for (const auto& Item : data) {
+        if (Item.id_item == input_id) {
+            cout << "Nama: " << Item.item_name << endl;
+            cout << "Harga: " << Item.item_price << endl;
+            ditemukan = true;
+            break;
+        }
+    }
+
+    // Jika barang tidak ditemukan
+    if (!ditemukan) {
+        cout << "Barang dengan ID " << input_id << " tidak ditemukan" << endl;
+    }
+}
+
 /* Main Program */
 int main(){
     cout << "********** MINIMARKET NAME **********" << endl;
     cout << "=====================================" << endl;
+    transaction();
 }
