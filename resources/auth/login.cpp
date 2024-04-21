2f29ef56798cd9d09a3f8bb0760ecfacfb9260c8
Steven Martua Christian Simbolon <144975699+stevendotexe@users.noreply.github.com>
2024-04-21 21:41:29 +0700

    login completed

--- resources/auth/login.cpp
+++ resources/auth/login.cpp
@@ -1,67 +1,43 @@
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
-#include <vector>
-#include <filesystem>
+#include "fileLoad.h"
 
 using namespace std;
-namespace filesystem = fs;
 
-int main(){
+tuple<bool, string, string> authenticate(vector<vector<string>> data){ // function to authenticate the user 
+    // 0: Account ID | 1: Staff Name | 2: User Level | 3: Username | 4: Password //
+    while (true){
+        string inputUsername, inputPassword, userPassword;
+        cout << "\nUsername\t: ";
+        getline(cin, inputUsername);
+        cout << "Password\t: ";
+        getline(cin, inputPassword);
+
+        for(int i = 0; i < data.size(); i++){ // iterate over data
+            if (data[i][3] == inputUsername){ 
+                if(data[i][4] == inputPassword){
+                    tuple<bool, string, string> authResult(true, inputUsername, data[i][2]);
+                    return authResult; // returns a tuple object
+                }
+            }
+        }
+        tuple<bool, string, string> fail(false, "-", "Username Not Found");
+        return fail;
+    }
+}
+
+void login(){
     cout << "======================================================================" << endl;
     cout << "Welcome to DataBarang, please wait a moment while we load some data..." << endl;
     cout << "======================================================================" << endl;
-    
-    fs::path currentDir = fs::current_path();
-    currentDir = currentDir.parent_path();
-    currentDir /= "database";
-    currentDir /= "login.csv";
 
-    vector<vector<string>> accountData = loadData(currentDir); // load account data into vector
+    vector<vector<string>> accountData = loadData("../../database/login.csv"); // load account data into vector
 
-    cout << ("Data loaded!\n");
+    cout << ("\nData loaded!\n");
+    tuple<bool, string, string> loginResult = authenticate(accountData);
+    bool loginStatus = get<0>(loginResult); // boolean to determine account is logged in or not
+    string username = get<1>(loginResult);
+    string accountType = get<2>(loginResult);
+
+    cout << "Selamat datang, " << accountType;
+
+    return 0;
 }
-
-
-vector<vector<string>> loadData(string filename) {
-    fstream theFile; // creates an fstream object for theFile
-    theFile.open(filename); //opens the file
-
-    if (!theFile.is_open()) {
-        cerr << "Error while opening file"; // print error message to standard error stream
-        return {}; // return an empty vector to indicate failure
-    }
-
-    vector<vector<string>> accountData; // vector<vector<string>> to store accountData
-
-    int linecount = 0; 
-    string line;
-    while (getline(theFile, line)) {
-        ++linecount; // increment linecount for each line
-
-        stringstream ss(line); // create a stringstream object for each line
-        vector<string> row;    // row -> a vector inside the vector to store each line
-
-        string field;
-        while (getline(ss, field, ',')) { // separate the data for each field and place it into row
-            row.push_back(field);
-        }
-        accountData.push_back(row); // Add the row to the accountData vector
-    }
-
-    theFile.close(); // closes the file
-
-    return accountData; // return the loaded data
-}
-
-void printData(const vector<vector<string>>& data) {
-    for (const auto& row : data) { // const auto (to automatically determine data type),
-                                   // and ampersand '&' to reference each element in accountData 
-        for (const auto& field : row) {
-            cout << field << "\t";
-        }
-        cout << endl;
-    }
-}
-
